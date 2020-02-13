3cb3054e8bc65255e66e95d325ffcc0011bf1185
Spencer Falor-Ward <srfalorward@ucdavis.edu>
2020-02-12 19:37:58 -0800

    added textRazor REST API code (#7)

--- group.js
+++ group.js
@@ -1,3 +1,24 @@
+//textRazor api code
+function testAPI() {
+  $.ajax({
+    url: " https://cors-anywhere.herokuapp.com/https://api.textrazor.com",
+    method: "POST",
+    timeout: 0,
+    headers: {
+      "x-textrazor-key":
+        "c8a64d61fcc45077687314db86948b8faf8546ee880318cd0d603f5f",
+      Type: "application/x-www-form-urlencoded"
+    },
+    data: {
+      extractors: "words,partOfSpeech",
+      text:
+        "Spain's stricken Bankia expects to sell off its vast portfolio of industrial holdings that includes a stake in the parent company of British Airways and Iberia."
+    }
+  }).done(function(response) {
+    console.log(response);
+  });
+}
+testAPI();
 
 
 
@@ -15,8 +36,8 @@ var emojiArray =[];
 // ajax query of the database- can be altered with above parameters
 $("#button").click(function() {
   var inputText = $("#inputField").val();
-  
-  var queryURL = "https://emoji-api.com/emojis" 
+
+  var queryURL = "https://emoji-api.com/emojis"
   $.ajax({
     url: queryURL,
     method: "GET"
@@ -24,11 +45,11 @@ $("#button").click(function() {
   .then(function(response){
     console.log(response);
   for (let i = 0; i < response.length; i++) {
-    // this creates an array of all of the emoji names 
+    // this creates an array of all of the emoji names
     emojiArray.push(response[i].slug);
     console.log(emojiArray);
-  }  
-    
+  }
+
 
   })
 })
