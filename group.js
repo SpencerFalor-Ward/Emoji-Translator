f4d77eb7190d727427252856819360928f02471b
Ryan Antonelli <58542460+Ryan-Antonelli@users.noreply.github.com>
2020-02-12 21:12:26 -0800

    Ryans branch (#8)
    * JS with a couple of potential APIs
    
    * ajax pull for emoji-api added and array for emoji names created
    
    * added emoji searcher
    
    Co-authored-by: Spencer Falor-Ward <srfalorward@ucdavis.edu>

--- group.js
+++ group.js
@@ -1,3 +1,4 @@
+
 //textRazor api code
 function testAPI() {
   $.ajax({
@@ -24,11 +25,58 @@ testAPI();
 
 
 
+
+
+var tester = ["face", "goog", "brexit"]
+
 // Emoji-api
 // https://emoji-api.com/emojis - emojis with group, subgroup, name, etc
 // https://emoji-api.com/emojis?search=computer - emoji search by keyword (computer in this case)
 // https://emoji-api.com/categories - emoji categories
 // https://emoji-api.com/categories/travel-places - all emojis within a single category
+
+
+// an array to populate with emoji names for searching
+var emojiArray =[];
+
+// ajax query of the database- can be altered with above parameters
+$("#button").click(function() {
+  var inputText = $("#inputField").val();
+  
+  var queryURL = "https://emoji-api.com/emojis" 
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+  .then(function(response){
+    console.log(response);
+  for (let i = 0; i < response.length; i++) {
+    // this creates an array of all of the emoji names 
+    emojiArray.push(response[i].slug);
+    
+    
+  }
+  console.log(emojiArray);
+  allIndexOf(emojiArray);  
+
+  })
+})
+
+var spain = "face"
+
+function allIndexOf (emojiArray) {
+  var indices = [];
+  for (let i = 0; i < emojiArray.length; i++) {
+    emojiYes = emojiArray[i].search("car")
+    if (emojiYes > -1) {
+      indices.push(i)
+    }
+  }
+  console.log(indices);
+}
+
+
+
 
 // an array to populate with emoji names for searching
 var emojiArray =[];
