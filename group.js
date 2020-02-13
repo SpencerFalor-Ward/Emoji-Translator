57cc3d542aab396dd4780278152c1e59c4c0848a
SpencerFW <srfalorward@ucdavis.edu>
2020-02-12 21:06:29 -0800

    added new extractors to textRazor API

--- group.js
+++ group.js
@@ -1,5 +1,5 @@
 //textRazor api code
-function testAPI() {
+function textRazorAPI() {
   $.ajax({
     url: " https://cors-anywhere.herokuapp.com/https://api.textrazor.com",
     method: "POST",
@@ -10,19 +10,15 @@ function testAPI() {
       Type: "application/x-www-form-urlencoded"
     },
     data: {
-      extractors: "words,partOfSpeech",
-      text:
-        "Spain's stricken Bankia expects to sell off its vast portfolio of industrial holdings that includes a stake in the parent company of British Airways and Iberia."
+      extractors: "words,partOfSpeech,param,relations, wikidataId",
+      text: $("#inputField").val()
+      // "Spain's stricken Bankia expects to sell off its vast portfolio of industrial holdings that includes a stake in the parent company of British Airways and Iberia."
     }
   }).done(function(response) {
     console.log(response);
   });
 }
-testAPI();
-
-
-
-
+textRazorAPI();
 
 // Emoji-api
 // https://emoji-api.com/emojis - emojis with group, subgroup, name, etc
@@ -31,27 +27,23 @@ testAPI();
 // https://emoji-api.com/categories/travel-places - all emojis within a single category
 
 // an array to populate with emoji names for searching
-var emojiArray =[];
+var emojiArray = [];
 
 // ajax query of the database- can be altered with above parameters
 $("#button").click(function() {
+  textRazorAPI(inputText);
   var inputText = $("#inputField").val();
 
-  var queryURL = "https://emoji-api.com/emojis"
+  var queryURL = "https://emoji-api.com/emojis";
   $.ajax({
     url: queryURL,
     method: "GET"
-  })
-  .then(function(response){
+  }).then(function(response) {
     console.log(response);
-  for (let i = 0; i < response.length; i++) {
-    // this creates an array of all of the emoji names
-    emojiArray.push(response[i].slug);
-    console.log(emojiArray);
-  }
-
-
-  })
-})
-
-
+    for (let i = 0; i < response.length; i++) {
+      // this creates an array of all of the emoji names
+      emojiArray.push(response[i].slug);
+    }
+  });
+  console.log(emojiArray);
+});
