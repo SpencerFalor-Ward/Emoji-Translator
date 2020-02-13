833142a23b5258ef5c6051ee3fc32b827ccb520a
SpencerFW <srfalorward@ucdavis.edu>
2020-02-12 19:21:04 -0800

    added textRazor REST API code

--- group.js
+++ group.js
@@ -1,46 +1,21 @@
-
-// emojidex
-// https://www.emojidex.com/api/v1
-// auth code 7abcc4a06e110eb981c96ab20eef352a93b099113001a4c5
-
-// var queryURLauth = "https://www.emojidex.com/api/v1/users/authenticate?username=RAntonelli&password=project1"
-
-// $.ajax({
-//     url: queryURLauth,
-//     method: "GET"
-//   })
-//   .then(function(response){
-//     console.log(response);
-    
-//   })
-
-// var queryURL = "https://www.emojidex.com/api/v1/categories"
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   })
-//   .then(function(response){
-//     console.log(response);      
-//   })
-
-// ----------------------------------------------------- 
-
-// https://emoji-api.com/emojis - emojis with group, subgroup, name, etc
-// https://emoji-api.com/emojis?search=computer - emoji search by keyword (computer in this case)
-// https://emoji-api.com/categories - emoji categories
-// https://emoji-api.com/categories/travel-places - single category
-
-// ----------------------------------------------------- 
-
-// Datamuse
-
-var queryURLdatamuse = "https://api.datamuse.com/words?ml=cat"
-var queryURLdatamuse1 = "https://api.datamuse.com/words?rel_trg=dog"
-
+//textRazor api code
+function testAPI() {
   $.ajax({
-    url: queryURLdatamuse1,
-    method: "GET"
-  })
-  .then(function(response){
-    console.log(response);      
-  })
+    url: " https://cors-anywhere.herokuapp.com/https://api.textrazor.com",
+    method: "POST",
+    timeout: 0,
+    headers: {
+      "x-textrazor-key":
+        "c8a64d61fcc45077687314db86948b8faf8546ee880318cd0d603f5f",
+      Type: "application/x-www-form-urlencoded"
+    },
+    data: {
+      extractors: "words,partOfSpeech",
+      text:
+        "Spain's stricken Bankia expects to sell off its vast portfolio of industrial holdings that includes a stake in the parent company of British Airways and Iberia."
+    }
+  }).done(function(response) {
+    console.log(response);
+  });
+}
+testAPI();
