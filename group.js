f42e979a3a1e80d0bc090f9d7d15adf1bad10c12
Ryan Antonelli <58542460+Ryan-Antonelli@users.noreply.github.com>
2020-02-15 13:33:35 -0800

    Ryans branch (#13)
    * JS with a couple of potential APIs
    
    * ajax pull for emoji-api added and array for emoji names created
    
    * added emoji searcher
    
    * emoji list creation and emoji selection code
    
    * retry
    
    * emoji search and placement

--- group.js
+++ group.js
@@ -1,3 +1,89 @@
+
+
+
+// an array to populate with emoji names for searching
+var emojiArray =[];
+var inputText = "red";
+var emojiList = [];
+// ajax query of the database- can be altered with above parameters
+// var inputText is a placeholder until more robust word-sliced input is created
+
+
+// this ajax query calls up all emojis who's name("code") contains the inputField text 
+$("#button").click(function(event) {
+  // var inputText = $("#inputField").val();
+  event.preventDefault();
+  var queryURL = `https://www.emojidex.com/api/v1/utf_emoji`
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+  .then(function(response){
+    emojiList = JSON.parse(response);
+    console.log(emojiList);
+    for (let i = 0; i < emojiList.length; i++) {
+          // this creates an array of all of the emoji names 
+          emojiArray.push(emojiList[i].code);            
+    }
+    console.log(emojiArray)
+    allIndexOf(emojiArray);
+  })
+})
+
+// selects all indexes with text containing words from the text input (needs word slicing functionality on text input to work)
+function allIndexOf (emojiArray) {
+  var indices = [];
+  for (let i = 0; i < emojiArray.length; i++) {
+    emojiYes = emojiArray[i].search(inputText);
+    if (emojiYes > -1) {
+      indices.push(i)
+    }
+  }
+  console.log(indices);
+  wordSlicer(indices);
+
+}
+
+
+// creating array with an object for each selected emoji containing split words, enabling a search differentiating "headscarf" from "car"
+function wordSlicer (indices) {
+  var matched = [];
+  for (let i = 0; i < indices.length; i++) {
+     var word = {full: emojiArray[indices[i]], broken: emojiArray[indices[i]].split(" ")}
+    console.log(word)
+    
+    // selecting items from the indices array which fail to meet the split word critereon and removing them from the array ie. "headscarf" is removed, but "car" remains
+     for (let j = 0; j < word.broken.length; j++) {
+         var wordMatch = word.broken[j]
+         console.log(wordMatch);
+
+         if(wordMatch === inputText){
+            matched.push(indices[i]);
+            break;
+          }
+      }
+               
+     
+  } 
+  console.log(matched);
+  console.log(indices);
+  emojiPlacer(matched);
+}
+
+// selecting a random index from within the indices array for a final emoji pick
+
+
+function emojiPlacer (matched) {
+
+  var EMOJI = matched[Math.floor(Math.random() * matched.length)]
+  console.log(EMOJI)
+  var spoon = $("#header").text(`&#x${emojiList[EMOJI].unicode};`)
+  console.log(spoon)
+}
+
+
+
+
 //textRazor api code
 function textRazorAPI() {
   $.ajax({
@@ -143,20 +229,20 @@ textRazorAPI();
 var emojiArray = [];
 
 // ajax query of the database- can be altered with above parameters
-$("#button").click(function() {
-  textRazorAPI(inputText);
-  var inputText = $("#inputField").val();
+// $("#button").click(function() {
+//   textRazorAPI(inputText);
+//   var inputText = $("#inputField").val();
 
-  var queryURL = "https://emoji-api.com/emojis";
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-    for (let i = 0; i < response.length; i++) {
-      // this creates an array of all of the emoji names
-      emojiArray.push(response[i].slug);
-    }
-  });
-  console.log(emojiArray);
-});
+//   var queryURL = "https://emoji-api.com/emojis";
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   }).then(function(response) {
+//     console.log(response);
+//     for (let i = 0; i < response.length; i++) {
+//       // this creates an array of all of the emoji names
+//       emojiArray.push(response[i].slug);
+//     }
+//   });
+//   console.log(emojiArray);
+// });
